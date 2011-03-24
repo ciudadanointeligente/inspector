a9448f6432441bfe372b75658211932afe1fc44e
José González Quintana <jgonzalezq@votainteligente.cl>
2011-03-24 17:28:20 -0300

    Registers presenters in PresenterInjector and PresenterModule
    Adds entries for Home, Main and Bill presenters
    
    Change-Id: Idca1bf8767eb0d0f57b21e2fe14cfbe12e1c04c7

--- src/cl/votainteligente/inspector/client/inject/PresenterInjector.java
+++ src/cl/votainteligente/inspector/client/inject/PresenterInjector.java
@@ -1,5 +1,9 @@
 package cl.votainteligente.inspector.client.inject;
 
+import cl.votainteligente.inspector.client.presenters.BillPresenter;
+import cl.votainteligente.inspector.client.presenters.HomePresenter;
+import cl.votainteligente.inspector.client.presenters.MainPresenter;
+
 import net.customware.gwt.presenter.client.EventBus;
 
 import com.google.gwt.inject.client.GinModules;
@@ -8,4 +12,7 @@ import com.google.gwt.inject.client.Ginjector;
 @GinModules(PresenterModule.class)
 public interface PresenterInjector extends Ginjector {
 	public EventBus getEventBus();
+	public MainPresenter getMainPresenter();
+	public HomePresenter getHomePresenter();
+	public BillPresenter getBillPresenter();
 }
--- src/cl/votainteligente/inspector/client/inject/PresenterModule.java
+++ src/cl/votainteligente/inspector/client/inject/PresenterModule.java
@@ -1,5 +1,12 @@
 package cl.votainteligente.inspector.client.inject;
 
+import cl.votainteligente.inspector.client.presenters.BillPresenter;
+import cl.votainteligente.inspector.client.presenters.HomePresenter;
+import cl.votainteligente.inspector.client.presenters.MainPresenter;
+import cl.votainteligente.inspector.client.views.BillView;
+import cl.votainteligente.inspector.client.views.HomeView;
+import cl.votainteligente.inspector.client.views.MainView;
+
 import net.customware.gwt.presenter.client.DefaultEventBus;
 import net.customware.gwt.presenter.client.EventBus;
 import net.customware.gwt.presenter.client.gin.AbstractPresenterModule;
@@ -10,5 +17,8 @@ public class PresenterModule extends AbstractPresenterModule {
 	@Override
 	protected void configure() {
 		bind(EventBus.class).to(DefaultEventBus.class).in(Singleton.class);
+		bindPresenter(MainPresenter.class, MainPresenter.Display.class, MainView.class);
+		bindPresenter(HomePresenter.class, HomePresenter.Display.class, HomeView.class);
+		bindPresenter(BillPresenter.class, BillPresenter.Display.class, BillView.class);
 	}
 }
