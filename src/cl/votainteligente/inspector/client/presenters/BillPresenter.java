675fc8b1495d933b002a02d2d716a0c7b6b0a3f2
José González Quintana <jgonzalezq@votainteligente.cl>
2011-03-25 18:03:49 -0300

    BillView Fixes and adds all the Bill model fields
    Change-Id: Ib7b2731b0c9e091e252ecbe1a890c3969c93d90c

--- src/cl/votainteligente/inspector/client/presenters/BillPresenter.java
+++ src/cl/votainteligente/inspector/client/presenters/BillPresenter.java
@@ -1,17 +1,42 @@
 package cl.votainteligente.inspector.client.presenters;
 
+import cl.votainteligente.inspector.client.inject.ServiceInjector;
+import cl.votainteligente.inspector.model.Bill;
+
 import net.customware.gwt.presenter.client.EventBus;
 import net.customware.gwt.presenter.client.widget.WidgetDisplay;
 import net.customware.gwt.presenter.client.widget.WidgetPresenter;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
+
+import java.util.Date;
 
 public class BillPresenter extends WidgetPresenter<BillPresenter.Display> implements BillPresenterIface {
 	public interface Display extends WidgetDisplay {
 		void setPresenter(BillPresenterIface presenter);
+		String getBillBulletinNumber();
+		void setBillBulletinNumber(String billBulletinNumber);
+		String getBillTitle();
+		void setBillTitle(String billTitle);
+		String getBillEntryDate();
+		void setBillEntryDate(Date billEntryDate);
+		String getBillInitiativeType();
+		void setBillInitiativeType(String billInitiativeType);
+		String getBillType();
+		void setBillType(String billType);
+		String getBillOriginChamber();
+		void setBillOriginChamber(String billOriginChamber);
+		String getBillUrgency();
+		void setBillUrgency(String billUrgency);
+		String getBillStage();
+		void setBillStage(String billStage);
 	}
 
 	private Long billId;
+	private static final ServiceInjector serviceInjector = GWT.create(ServiceInjector.class);
 
 	@Inject
 	public BillPresenter(Display display, EventBus eventBus) {
@@ -47,7 +72,24 @@ public class BillPresenter extends WidgetPresenter<BillPresenter.Display> implem
 
 	@Override
 	public void showBill() {
-		// TODO: fill this method
-	}
+		serviceInjector.getBillService().getBill(billId, new AsyncCallback<Bill>() {
 
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("No se ha podido cargar el proyecto de ley");
+			}
+
+			@Override
+			public void onSuccess(Bill result) {
+				display.setBillBulletinNumber(result.getBulletinNumber());
+				display.setBillTitle(result.getTitle());
+				display.setBillEntryDate(result.getEntryDate());
+				display.setBillInitiativeType(result.getInitiativeType().getName());
+				display.setBillType(result.getBillType().getName());
+				display.setBillOriginChamber(result.getOriginChamber().getName());
+				display.setBillUrgency(result.getUrgency().getName());
+				display.setBillStage(result.getStage().getName());
+			}
+		});
+	}
 }
--- src/cl/votainteligente/inspector/client/views/BillView.java
+++ src/cl/votainteligente/inspector/client/views/BillView.java
@@ -6,6 +6,7 @@ import cl.votainteligente.inspector.model.Parlamentarian;
 import cl.votainteligente.inspector.model.Society;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.cellview.client.CellTable;
@@ -13,14 +14,20 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
+import java.util.Date;
+
 public class BillView extends Composite implements BillPresenter.Display {
 	private static BillViewUiBinder uiBinder = GWT.create(BillViewUiBinder.class);
 	interface BillViewUiBinder extends UiBinder<Widget, BillView> {}
 
-	@UiField Label billName;
-	@UiField Label billDate;
-	@UiField Label billVote;
-	@UiField Label billStatus;
+	@UiField Label billBulletinNumber;
+	@UiField Label billTitle;
+	@UiField Label billEntryDate;
+	@UiField Label billInitiativeType;
+	@UiField Label billType;
+	@UiField Label billOriginChamber;
+	@UiField Label billUrgency;
+	@UiField Label billStage;
 	@UiField CellTable<Parlamentarian> parlamentarianTable;
 	@UiField CellTable<Society> societyTable;
 
@@ -39,4 +46,86 @@ public class BillView extends Composite implements BillPresenter.Display {
 	public void setPresenter(BillPresenterIface presenter) {
 		this.presenter = presenter;
 	}
+
+	@Override
+	public String getBillBulletinNumber() {
+		return billBulletinNumber.getText();
+	}
+
+	@Override
+	public void setBillBulletinNumber(String billBulletinNumber) {
+		this.billBulletinNumber.setText(billBulletinNumber);
+	}
+
+	@Override
+	public String getBillTitle() {
+		return billTitle.getText();
+	}
+
+	@Override
+	public void setBillTitle(String billTitle) {
+		this.billTitle.setText(billTitle);
+	}
+
+	@Override
+	public String getBillEntryDate() {
+		return billEntryDate.getText();
+	}
+
+	@Override
+	public void setBillEntryDate(Date billEntryDate) {
+		this.billEntryDate.setText(DateTimeFormat.getFormat("dd/MM/yyyy HH:mm").format(billEntryDate));
+	}
+
+	@Override
+	public String getBillInitiativeType() {
+		return billInitiativeType.getText();
+	}
+
+	@Override
+	public void setBillInitiativeType(String billInitiativeType) {
+		this.billInitiativeType.setText(billInitiativeType);
+	}
+
+	@Override
+	public String getBillType() {
+		return billType.getText();
+	}
+
+	@Override
+	public void setBillType(String billType) {
+		this.billType.setText(billType);
+	}
+
+
+	@Override
+	public String getBillOriginChamber() {
+		return billOriginChamber.getText();
+	}
+
+	@Override
+	public void setBillOriginChamber(String billOriginChamber) {
+		this.billOriginChamber.setText(billOriginChamber);
+	}
+
+	@Override
+	public String getBillUrgency() {
+		return billUrgency.getText();
+	}
+
+	@Override
+	public void setBillUrgency(String billUrgency) {
+		this.billStage.setText(billUrgency);
+	}
+
+	@Override
+	public String getBillStage() {
+		return billStage.getText();
+	}
+
+	@Override
+	public void setBillStage(String billStage) {
+		this.billStage.setText(billStage);
+	}
 }
+
