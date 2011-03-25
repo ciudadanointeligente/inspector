47242ab345bb50eb5ed3e58c92cee5a244713623
José González Quintana <jgonzalezq@votainteligente.cl>
2011-03-25 15:26:58 -0300

    Registers Services in Service Injector and Module
    Change-Id: I132a965eef6d1194af69bf30ea7c5b66433322c2

--- src/cl/votainteligente/inspector/client/inject/ServiceModule.java
+++ src/cl/votainteligente/inspector/client/inject/ServiceModule.java
@@ -1,6 +1,6 @@
 package cl.votainteligente.inspector.client.inject;
 
-import cl.votainteligente.inspector.client.services.PartyServiceAsync;
+import cl.votainteligente.inspector.client.services.*;
 
 import com.google.gwt.inject.client.AbstractGinModule;
 import com.google.inject.Singleton;
@@ -8,6 +8,21 @@ import com.google.inject.Singleton;
 public class ServiceModule extends AbstractGinModule {
 	@Override
 	protected void configure() {
+		bind(BillServiceAsync.class).in(Singleton.class);
+		bind(BillTypeServiceAsync.class).in(Singleton.class);
+		bind(CategoryServiceAsync.class).in(Singleton.class);
+		bind(ChamberServiceAsync.class).in(Singleton.class);
+		bind(CommissionServiceAsync.class).in(Singleton.class);
+		bind(DistrictServiceAsync.class).in(Singleton.class);
+		bind(DistrictTypeServiceAsync.class).in(Singleton.class);
+		bind(InitiativeTypeServiceAsync.class).in(Singleton.class);
+		bind(NotaryServiceAsync.class).in(Singleton.class);
+		bind(ParlamentarianServiceAsync.class).in(Singleton.class);
 		bind(PartyServiceAsync.class).in(Singleton.class);
+		bind(PersonServiceAsync.class).in(Singleton.class);
+		bind(SocietyServiceAsync.class).in(Singleton.class);
+		bind(SocietyTypeServiceAsync.class).in(Singleton.class);
+		bind(StageServiceAsync.class).in(Singleton.class);
+		bind(UrgencyServiceAsync.class).in(Singleton.class);
 	}
 }
