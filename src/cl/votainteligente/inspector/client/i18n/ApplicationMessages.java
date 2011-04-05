a42897fa198e2192ac806fc7699e4cf811bd0422
José González Quintana <jgonzalezq@votainteligente.cl>
2011-04-05 15:25:15 -0300

    Adds HomeView messages
    Change-Id: I3b1965de8fa42d0820e4f001b314faf2e30f0a3f

--- src/cl/votainteligente/inspector/client/i18n/ApplicationMessages.java
+++ src/cl/votainteligente/inspector/client/i18n/ApplicationMessages.java
@@ -3,6 +3,73 @@ package cl.votainteligente.inspector.client.i18n;
 import com.google.gwt.i18n.client.Messages;
 
 public interface ApplicationMessages extends Messages {
+
+	@Key("general.bill")
+	public String getGeneralBill();
+
+	@Key("general.category")
+	public String getGeneralCategory();
+
+	@Key("general.district")
+	public String getGeneralDistrict();
+
+	@Key("general.parlamentarian")
+	public String getGeneralParlamentarian();
+
+	@Key("general.party")
+	public String getGeneralParty();
+
+	@Key("general.society")
+	public String getGeneralSociety();
+
+	@Key("general.suscribe")
+	public String getGeneralSusbcribe();
+
+	@Key("general.profile")
+	public String getGeneralProfile();
+
+	@Key("general.title")
+	public String getGeneralTitle();
+
+	@Key("error.parlamentarian.list")
+	public String getErrorParlamentarianList();
+
+	@Key("error.parlamentarian.search")
+	public String getErrorParlamentarianSearch();
+
+	@Key("error.parlamentarian.categorySearch")
+	public String getErrorParlamentarianCategorySearch();
+
+	@Key("error.category.list")
+	public String getErrorCategoryList();
+
+	@Key("error.category.search")
+	public String getErrorCategorySearch();
+
+	@Key("error.category.parlamentarianSearch")
+	public String getErrorCategoryParlamentarianSearch();
+
+	@Key("error.bill.list")
+	public String getErrorBillList();
+
+	@Key("bill.bulletin")
+	public String getBillBulletin();
+
+	@Key("parlamentarian.societies")
+	public String getParlamentarianSocieties();
+
+	@Key("parlamentarian.authoredBills")
+	public String getParlamentarianAuthoredBills();
+
+	@Key("parlamentarian.votedBills")
+	public String getParlamentarianVotedBills();
+
+	@Key("parlamentarian.interestDeclarationFile")
+	public String getParlamentarianInterestDeclarationFile();
+
+	@Key("parlamentarian.patrimonyDeclarationFile")
+	public String getParlamentarianPatrimonyDeclarationFile();
+
 	@Key("person.civilStatusSingle")
 	public String getCivilStatusSingle();
 
