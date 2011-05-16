c3862c94510db8d3378fa3832646ffe0b95e3def
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2011-05-16 12:19:59 -0400

    Creates ResourceBundle in views package
    Change-Id: Iddf2324d6fa413e19ba5691251d606f4e3e6e3e2

---
+++ src/cl/votainteligente/inspector/client/views/ResourceBundle.java
@@ -0,0 +1,11 @@
+package cl.votainteligente.inspector.client.views;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+
+public interface ResourceBundle extends ClientBundle {
+	public static final ResourceBundle INSTANCE = GWT.create(ResourceBundle.class);
+
+	@Source("HomeView.css")
+	HomeViewCss HomeView();
+}
