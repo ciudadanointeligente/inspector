1d4e8f43192115110c4b96f482e9f5cfddfba43d
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2011-07-29 16:21:53 -0400

    ParlamentarianComment: Adds model
    Change-Id: I2b865738d1beb7d76d04e5db7cacec77eccbb8c1

---
+++ src/cl/votainteligente/inspector/model/ParlamentarianComment.java
@@ -0,0 +1,98 @@
+package cl.votainteligente.inspector.model;
+
+import net.sf.gilead.pojo.gwt.LightEntity;
+
+import java.util.Date;
+
+public class ParlamentarianComment extends LightEntity implements Comparable<ParlamentarianComment> {
+	private Long id;
+	private Parlamentarian parlamentarian;
+	private String key;
+	private String subject;
+	private String body;
+	private Date creationDate;
+	private Boolean aproved;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Parlamentarian getParlamentarian() {
+		return parlamentarian;
+	}
+
+	public void setParlamentarian(Parlamentarian parlamentarian) {
+		this.parlamentarian = parlamentarian;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Boolean getAproved() {
+		return aproved;
+	}
+
+	public void setAproved(Boolean aproved) {
+		this.aproved = aproved;
+	}
+
+	@Override
+	public int compareTo(ParlamentarianComment obj) {
+		return (obj == null || obj.getCreationDate() == null) ? -1 : -obj.getCreationDate().compareTo(creationDate);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == null) {
+			return false;
+		}
+
+		if (!(other instanceof Society)) {
+			return false;
+		}
+
+		if (getId() == null) {
+			return false;
+		}
+
+		return getId().equals(((Society) other).getId());
+	}
+
+	@Override
+	public int hashCode() {
+			return this.id.hashCode();
+	}
+}
